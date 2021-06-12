bf2a50f721bf1955403517c7b6054af68256f348
Sharoon Rafeek <sharoonrafeek@gmail.com>
2021-06-12 16:26:30 +0530

    time fixes

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ while True:
     hour = now.hour
     minute = now.minute
 
-    if hour == 16 and minute == 24 and posted == False:
+    if hour == 9 and minute == 0 and posted == False:
         api = tweepy.API(auth)
         nasa_api = environ['NASA_API']
         response = requests.get(nasa_api)
@@ -63,5 +63,5 @@ while True:
 
         posted = True
 
-    if hour == 16 and minute == 27:
+    if hour == 9 and minute == 5:
         posted = False
