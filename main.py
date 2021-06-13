a3357ae2ac4a88d83052cf9928a6c82dfa213af6
Sharoon Rafeek <sharoonrafeek@gmail.com>
2021-06-13 09:50:53 +0530

    changes for video format

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ while True:
     hour = now.hour
     minute = now.minute
 
-    if hour == 18 and minute == 0 and posted == False:
+    if hour == 9 and minute == 53 and posted == False:
         api = tweepy.API(auth)
         nasa_api = environ['NASA_API']
         response = requests.get(nasa_api)
